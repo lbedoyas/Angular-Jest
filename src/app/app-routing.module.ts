b2d87f1c93693e2cdf1b3d8107ab58b6095a6016
Luis Bedoya <luis.bedoya@bhdleon.com.do>
2022-04-07 23:15:15 -0500

    Estructura de components father and son
    Estructura de components father and son

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharizardComponent } from './basic/charizard/charizard.component';
 import { CounterComponent } from './basic/counter/counter.component';
+import { FatherComponent } from './basic/father/father.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'basic/charizard', component: CharizardComponent
+  },
+  {
+    path: 'basic/father', component: FatherComponent
   },
   {
     path: '**',
